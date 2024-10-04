b5053826fc88142c9ba752fdf3c182563b2d8c07
dragonyongs <47410020+dragonyongs@users.noreply.github.com>
2024-10-04 23:27:21 +0900

    Transaction.js 업데이트
    expense_type TeamCard 추가

--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -9,7 +9,7 @@ const transactionSchema = new mongoose.Schema({
     transaction_type: { type: String, enum: ['expense', 'income'], required: true },
     deposit_type: { 
         type: String, 
-        enum: ['RegularDeposit', 'TransportationExpense', 'TeamFund', 'AdditionalDeposit'], 
+        enum: ['RegularDeposit', 'TransportationExpense', 'TeamFund', 'TeamCard', 'AdditionalDeposit'], 
         required: function() {
             return this.transaction_type === 'income'; // deposit일 경우에만 필수
         }
