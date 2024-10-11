3b460b4b1b844957e3ed932b1ef2fe7a7389c0df
dragonyongs <47410020+dragonyongs@users.noreply.github.com>
2024-10-11 23:51:39 +0900

    Profile.js 업데이트

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,10 +1,9 @@
-const mongoose = require('mongoose');
-
 const profileSchema = new mongoose.Schema({
-    member_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Member', required: true }, // Member와 연결
+    member_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Member', required: true }, 
     phones: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Phone' }],
     dates: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Date' }],
-    addresses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Address' }]
+    addresses: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Address' }],
+    avatar_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Avatar', default: null } // 아바타 참조 추가
 }, { timestamps: true });
 
 const Profile = mongoose.model('Profile', profileSchema);
