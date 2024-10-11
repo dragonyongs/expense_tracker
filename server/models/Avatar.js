fb3d3909d3ee6c893b53c9740a09a8b545d79c20
dragonyongs <47410020+dragonyongs@users.noreply.github.com>
2024-10-11 23:52:12 +0900

    Avatar.js 업데이트

--- server/models/Avatar.js
+++ server/models/Avatar.js
@@ -1,22 +1,19 @@
-const mongoose = require('mongoose');
-
 const avatarSchema = new mongoose.Schema({
     sex: { type: String, enum: ['man', 'woman'], required: true },
     faceColor: { type: String, required: true },
     earSize: { type: String, enum: ['small', 'big'], required: true },
     eyeStyle: { type: String, enum: ['circle', 'oval', 'smile'], required: true },
     noseStyle: { type: String, enum: ['short', 'long', 'round'], required: true },
-    mouthStyle: { type: String, enum: ['laugh', 'smile', 'peace'], required: true },  // 수정된 부분
-    shirtStyle: { type: String, enum: ['hoody', 'short', 'polo'], required: true },   // 수정된 부분
+    mouthStyle: { type: String, enum: ['laugh', 'smile', 'peace'], required: true },
+    shirtStyle: { type: String, enum: ['hoody', 'short', 'polo'], required: true },
     glassesStyle: { type: String, enum: ['none', 'round', 'square'], required: true },
     hairColor: { type: String, required: true },
-    hairStyle: { type: String, enum: ['normal', 'thick', 'mohawk', 'womanLong', 'womanShort'], required: true }, // 수정된 부분
-    hatStyle: { type: String, enum: ['none', 'beanie', 'turban'], required: true },  // 수정된 부분
+    hairStyle: { type: String, enum: ['normal', 'thick', 'mohawk', 'womanLong', 'womanShort'], required: true },
+    hatStyle: { type: String, enum: ['none', 'beanie', 'turban'], required: true },
     hatColor: { type: String, required: false },
     shirtColor: { type: String, required: true },
     bgColor: { type: String, required: true },
-    member_id: { type: String, required: true }, // member_id는 String으로 유지
-    // member_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Member', required: true } // 멤버 참조
+    member_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Member', required: true }, // `ObjectId`로 변경
 }, { timestamps: true });
 
 const Avatar = mongoose.model('Avatar', avatarSchema);
