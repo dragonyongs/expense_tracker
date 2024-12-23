090ff4b08abe14ab5e99fe6c2c940d3be33206f9
ADY <ujudge@naver.com>
2024-12-23 18:19:57 +0900

    다크모드 metaTag Theme 컬러 변경

--- client/src/context/ThemeColorContext.jsx
+++ client/src/context/ThemeColorContext.jsx
@@ -1,16 +1,25 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
+import { useDarkMode } from './DarkModeContext';
 
 const ThemeColorContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
     const [themeColor, setThemeColor] = useState("#0433FF"); // 기본 색상 설정
+    const { isDarkMode } = useDarkMode();
 
     useEffect(() => {
         const metaTag = document.querySelector("meta[name='theme-color']");
         if (metaTag) {
             metaTag.setAttribute("content", themeColor); // 메타 태그 업데이트
         }
-    }, [themeColor]); // themeColor가 변경될 때마다 실행
+
+        if( isDarkMode ) {
+            setThemeColor('#1d293b');
+        } else {
+            setThemeColor("#0433FF");
+        }
+
+    }, [themeColor, isDarkMode]); // themeColor가 변경될 때마다 실행
 
     return (
         <ThemeColorContext.Provider value={{ themeColor, setThemeColor }}>
