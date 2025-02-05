ec44bdaf2f76afc3ef191eb6014a3999089109da
ADY <ujudge@naver.com>
2025-02-05 10:05:00 +0900

    카드 잔액 완료 후 드로우 상태, 애니메이션 효과 상위 대시보드로 이동 및 코드정리/개선

--- client/src/components/CardBalance.jsx
+++ client/src/components/CardBalance.jsx
@@ -1,41 +1,23 @@
-import React, { useState} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { ConfettiEffect } from './ConfettiEffect';
 import TransactionDrawer from './TransactionDrawer';
 import AnimatedNumber from './AnimatedNumber';
 import { TiPlus } from "react-icons/ti";
 
-function CardBalance({ onSave, onDelete, currentBalance, teamFund, userCards, cardBalance, errMsg }) {
-    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-    const [confettiTrigger, setConfettiTrigger] = useState(false);
-
-    const handleSave = async (transactionData) => {
-        try {
-            await onSave(transactionData); // 부모로 저장 요청 전달
-        } catch (error) {
-            console.error("CardBalance - Error saving transaction:", error);
-        } finally {
-            setConfettiTrigger(true);
-            setTimeout(() => setConfettiTrigger(false), 3000);
-            setIsDrawerOpen(false);
-            // 카드 지출 오픈 상태는 Dashboard로 이동해야함
-        }
-    };
-    
-    const handleDelete = async (transactionId) => {
-        try {
-            await onDelete(transactionId);
-            setIsDrawerOpen(false);
-        } catch (error) {
-            console.error('Error deleting transaction:', error);
-        }
-    };
-
-    const handleOpenDrawer = () => {
-        // setErrMsg('');
-        setIsDrawerOpen(true)
-    };
-    const handleCloseDrawer = () => setIsDrawerOpen(false);
+function CardBalance({ 
+    currentBalance, 
+    teamFund, 
+    userCards, 
+    cardBalance, 
+    errMsg, 
+    onOpenDrawer, 
+    isDrawerOpen, 
+    onSave, 
+    onDelete, 
+    confettiTrigger, 
+    onCloseDrawer 
+}) {
 
     return (
         <div className='flex flex-col items-center gap-y-3 pt-6 pb-4 rounded-es-4xl rounded-ee-4xl bg-[#0433FF]'>
@@ -50,7 +32,7 @@ function CardBalance({ onSave, onDelete, currentBalance, teamFund, userCards, ca
             </div>
             <div className='flex justify-center items-center gap-x-3 mt-8'>
                 <button
-                    onClick={handleOpenDrawer}
+                    onClick={onOpenDrawer}
                     className='inline-flex items-center gap-x-2 py-2 px-6 border-2 border-blue-100 rounded-full text-white'
                 >
                     <TiPlus /> 카드 지출
@@ -58,115 +40,34 @@ function CardBalance({ onSave, onDelete, currentBalance, teamFund, userCards, ca
                 <ConfettiEffect triggerConfetti={confettiTrigger} />
             </div>
 
-            {/* 트랜잭션 드로어 컴포넌트 */}
             <TransactionDrawer
                 isOpen={isDrawerOpen}
-                onClose={handleCloseDrawer}
-                onSave={handleSave} // 저장 요청 전달
-                onDelete={handleDelete} // 삭제 요청 전달
-                userCards={userCards} // 카드 정보
-                isEditing={false} // 트랜잭션 수정 모드가 아님
-                transactionData={{}} // 새로운 트랜잭션 데이터
-                errMsg={errMsg} // 에러 메시지
-                cardBalance={cardBalance} // 카드 잔액
-                teamFund={teamFund || 0} // 팀 펀드
+                onClose={onCloseDrawer}
+                onSave={onSave}
+                onDelete={onDelete}
+                userCards={userCards}
+                isEditing={false}
+                transactionData={{}}
+                errMsg={errMsg}
+                cardBalance={cardBalance}
+                teamFund={teamFund || 0}
             />
         </div>
     );
 }
 
 CardBalance.propTypes = {
-    onSave: PropTypes.func.isRequired, // 저장 요청 함수
-    onDelete: PropTypes.func.isRequired, // 삭제 요청 함수
-    currentBalance: PropTypes.number.isRequired, // 카드 잔액
-    teamFund: PropTypes.number, // 팀 펀드
-    cardBalance: PropTypes.number, // 팀카드 잔액
-    userCards: PropTypes.array.isRequired, // 카드 정보
+    onSave: PropTypes.func.isRequired,
+    onDelete: PropTypes.func.isRequired,
+    currentBalance: PropTypes.number.isRequired,
+    teamFund: PropTypes.number,
+    cardBalance: PropTypes.number,
+    userCards: PropTypes.array.isRequired,
     errMsg: PropTypes.string,
-    setErrMsg: PropTypes.func,
+    isDrawerOpen: PropTypes.bool.isRequired,
+    confettiTrigger: PropTypes.bool.isRequired,
+    onOpenDrawer: PropTypes.func.isRequired,
+    onCloseDrawer: PropTypes.func.isRequired
 };
 
 export default CardBalance;
-
-// import React, { useEffect, useState, useContext } from 'react';
-// import PropTypes from 'prop-types';
-// import { AuthContext } from '../context/AuthProvider';
-// import { ConfettiEffect } from './ConfettiEffect';
-// import TransactionDrawer from './TransactionDrawer';
-// import AnimatedNumber from './AnimatedNumber';
-// import { TiPlus } from "react-icons/ti";
-
-// function CardBalance({ onSave, currentBalance, teamFund, userCards, cardBalance }) {
-//     const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-//     const { user } = useContext(AuthContext);
-//     const [confettiTrigger, setConfettiTrigger] = useState(false);
-
-//     const handleSave = async (transactionData) => {
-//         try {
-//             onSave(transactionData);
-
-//             setIsDrawerOpen(false);
-//             setConfettiTrigger(true);
-
-//             setTimeout(() => {
-//                 setConfettiTrigger(false);
-//             }, 3000);
-//         } catch (error) {
-//             console.error('Error saving transaction:', error);
-//         }
-//     };
-
-//     const handleOpenDrawer = () => setIsDrawerOpen(true);
-//     const handleCloseDrawer = () => setIsDrawerOpen(false);
-    
-//     return (
-//         <div className='flex flex-col items-center gap-y-3 pt-6 pb-4 rounded-es-4xl rounded-ee-4xl bg-[#0433FF]'>
-//             <p className='text-xl text-blue-200'>
-//                 카드 잔액 {5000 > currentBalance ? "🥲" : 10000 >= currentBalance ? "😱" : "🤑"}
-//             </p>
-//             <div className='flex justify-center items-center gap-x-2 text-white text-5xl tracking-tighter'>
-//                 <span className="font-thin">₩</span>
-//                 <p className="font-semibold">
-//                     <AnimatedNumber value={currentBalance} />
-//                 </p>
-//             </div>
-//             <div className='flex justify-center items-center gap-x-3 mt-10'>
-//                 <button
-//                     onClick={handleOpenDrawer}
-//                     className='inline-flex items-center gap-x-2 py-3 px-10 border-2 border-blue-100 rounded-full text-white'
-//                 >
-//                     <TiPlus /> 카드 지출
-//                 </button>
-//                 <ConfettiEffect triggerConfetti={confettiTrigger} />
-//             </div>
-
-//             {/* 트랜잭션 드로어 컴포넌트 */}
-//             <TransactionDrawer
-//                 isOpen={isDrawerOpen}
-//                 onClose={handleCloseDrawer}
-//                 onSave={handleSave}
-//                 userCards={userCards} // 카드 정보
-//                 isEditing={false} // 트랜잭션 수정 모드가 아님
-//                 transactionData={{}} // 새로운 트랜잭션 데이터
-//                 errMsg={null} // 에러 메시지
-//                 // user={user} // 사용자 정보
-//                 cardBalance={cardBalance} // 카드 잔액 (롤오버 금액과 팀 펀드를 포함한 잔액)
-//                 teamFund={teamFund || 0} // 팀 펀드 (부모에서 전달)
-//                 onDelete={(data) => {
-//                     console.log("Transaction deleted:", data);
-//                     handleCloseDrawer();
-//                 }} 
-//             />
-//         </div>
-//     );
-// }
-
-// CardBalance.propTypes = {
-//     onSave: PropTypes.func.isRequired,  // 트랜잭션 저장 함수
-//     currentBalance: PropTypes.number.isRequired, // 카드 잔액
-//     teamFund: PropTypes.number, // 팀 펀드
-//     cardBalance: PropTypes.number, // 팀카드 잔액
-//     userCards: PropTypes.array.isRequired, // 카드 정보
-// };
-
-// export default CardBalance;
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -6,37 +6,6 @@ import Header from '../components/Header';
 import axios from "../services/axiosInstance";
 import { API_URLS } from '../services/apiUrls';
 
-const useFetchData = (fetchFunction) => {
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState('');
-
-    const fetchData = async () => {
-        setIsLoading(true);
-        try {
-            await fetchFunction();
-            setError(null);
-        } catch (err) {
-            setError(handleError(err));
-        } finally {
-            setIsLoading(false);
-        }
-    };
-
-    return { fetchData, isLoading, error };
-};
-
-const handleError = (error) => {
-    if (error.response) {
-        return error.response.data.message || "오류가 발생했습니다.";
-    } else if (error.message) {
-        return error.message || "오류가 발생했습니다.";
-    } else if (error.request) {
-        return "서버로부터 응답을 받지 못했습니다. 네트워크 문제일 수 있습니다.";
-    } else {
-        return "알 수 없는 오류가 발생했습니다.";
-    }
-};
-
 const Dashboard = () => {
     const { user } = useContext(AuthContext);
     const [transactions, setTransactions] = useState([]);
@@ -45,56 +14,42 @@ const Dashboard = () => {
     const [userCards, setUserCards] = useState([]);
     const [teamFund, setTeamFund] = useState(0);
     const [errMsg, setErrMsg] = useState('');
-    const [cacheNotification, setCacheNotification] = useState(''); // 캐시 알림 상태 추가
+    const [confettiTrigger, setConfettiTrigger] = useState(false);
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
-    const { fetchData: fetchTransactions, isLoading: isLoadingTransactions, error: transactionError } = useFetchData(async () => {
-        const response = await axios.get(API_URLS.TRANSACTIONS);
-        setTransactions(response.data);
-    });
+    const fetchTransactions = async () => {
+        try {
+            const response = await axios.get(API_URLS.TRANSACTIONS);
+            setTransactions(response.data);
+        } catch (error) {
+            setErrMsg(error.response?.data?.message || '오류가 발생했습니다.');
+        }
+    };
 
-    const { fetchData: fetchUserCard, isLoading: isLoadingUserCard } = useFetchData(async () => {
+    const fetchUserCard = async () => {
         if (user?.member_id) {
-            const response = await axios.get(`${API_URLS.CARD_MEMBER}/${user.member_id}`);
-            if (response.data.length > 0) {
-                const userCard = response.data[0];
-                const currentBalanceWithRollover = userCard.balance + (userCard.rollover_amount || 0) + (userCard.team_fund || 0);
-                setCurrentBalance(currentBalanceWithRollover);
-                setUserCards(response.data);
-                setCardBalance(userCard.balance || 0);
-                setTeamFund(userCard.team_fund || 0);
+            try {
+                const response = await axios.get(`${API_URLS.CARD_MEMBER}/${user.member_id}`);
+                if (response.data.length > 0) {
+                    const userCard = response.data[0];
+                    const currentBalanceWithRollover = userCard.balance + (userCard.rollover_amount || 0) + (userCard.team_fund || 0);
+                    setCurrentBalance(currentBalanceWithRollover);
+                    setUserCards(response.data);
+                    setCardBalance(userCard.balance || 0);
+                    setTeamFund(userCard.team_fund || 0);
+                }
+            } catch (error) {
+                setErrMsg(error.response?.data?.message || '오류가 발생했습니다.');
             }
         }
-    });
-
-    // 서비스 워커 메시지 수신
-    useEffect(() => {
-        if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.addEventListener('message', (event) => {
-                if (event.data && event.data.type === "CACHE_USED") {
-                    setCacheNotification(`캐시된 데이터를 사용 중입니다: ${event.data.url}`);
-                }
-            });
-        }
-    }, []);
-
-    // 캐시 알림 자동 숨김
-    useEffect(() => {
-        if (cacheNotification) {
-            const timer = setTimeout(() => setCacheNotification(''), 5000); // 5초 후 알림 사라짐
-            return () => clearTimeout(timer);
-        }
-    }, [cacheNotification]);
+    };
 
     useEffect(() => {
         fetchTransactions();
-    }, []);
-
-    useEffect(() => {
         fetchUserCard();
     }, [user?.member_id]);
 
     const handleSaveTransaction = async (newTransaction) => {
-
         const transactionData = {
             card_id: newTransaction.card_id,
             expense_card: newTransaction.expense_card,
@@ -113,36 +68,29 @@ const Dashboard = () => {
             } else {
                 await axios.post(API_URLS.TRANSACTIONS, transactionData);
             }
-
             await fetchTransactions();
             await fetchUserCard(); 
+            setConfettiTrigger(true);
+            setTimeout(() => setConfettiTrigger(false), 3000);
         } catch (error) {
-            console.error("handleSaveTransaction-Error saving transaction:", error);
-            setErrMsg(handleError(error));
+            setErrMsg(error.response?.data?.message || '오류가 발생했습니다.');
         }
     };
 
     const handleDeleteTransaction = async (transactionId) => {
         try {
             await axios.delete(`${API_URLS.TRANSACTIONS}/${transactionId}`);
-    
             await fetchTransactions();
             await fetchUserCard();
         } catch (error) {
-            console.error("Error deleting transaction:", error);
-            setErrMsg(handleError(error));
+            setErrMsg(error.response?.data?.message || '오류가 발생했습니다.');
         }
     };
 
     return (
         <>
             <Header />
-            <div className='flex flex-col w-full'>  
-                {cacheNotification && (
-                    <div className="fixed bottom-4 right-4 bg-yellow-500 text-black px-4 py-2 rounded shadow-md">
-                        {cacheNotification}
-                    </div>
-                )}
+            <div className='flex flex-col w-full'>
                 {user.role === 'super_admin' ? (
                     <div className='p-8'>
                         <p>{user.role}</p>
@@ -157,13 +105,15 @@ const Dashboard = () => {
                             userCards={userCards}
                             cardBalance={cardBalance}
                             errMsg={errMsg}
-                            // setErrMsg={setErrMsg}
+                            isDrawerOpen={isDrawerOpen}
+                            confettiTrigger={confettiTrigger}
+                            onOpenDrawer={() => setIsDrawerOpen(true)}
+                            onCloseDrawer={() => setIsDrawerOpen(false)}
                         />
                         <PayHistory
                             transactions={transactions}
                             onDelete={handleDeleteTransaction}
                             userCards={userCards}
-                            isLoading={isLoadingTransactions || isLoadingUserCard}
                             errMsg={errMsg}
                             setErrMsg={setErrMsg}
                         />
@@ -175,117 +125,3 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
-
-    // import { IoShareOutline } from "react-icons/io5";
-
-    {/* <InstallButton /> */}
-    // const [deferredPrompt, setDeferredPrompt] = useState(null);
-    // const [installState, setInstallState] = useState({
-    //     isIOS: false,
-    //     isSafari: false,
-    //     isChrome: false,
-    //     isStandalone: false,
-    //     showInstallPrompt: false,
-    // });
-
-    // useEffect(() => {
-    //     const detectBrowser = () => {
-    //         const ua = window.navigator.userAgent;
-    //         const iOS = /iPad|iPhone|iPod/.test(ua);
-    //         const isSafari = /Safari/.test(ua) && !/Chrome/.test(ua);
-    //         const isChrome = /Chrome/.test(ua);
-    //         const isStandalone = window.matchMedia('(display-mode: standalone)').matches || 
-    //                             window.navigator.standalone || 
-    //                             document.referrer.includes('android-app://');
-
-    //         setInstallState({
-    //             isIOS: iOS,
-    //             isSafari: isSafari,
-    //             isChrome: isChrome,
-    //             isStandalone: isStandalone,
-    //             showInstallPrompt: !isStandalone && (iOS || deferredPrompt !== null),
-    //         });
-    //     };
-
-    //     detectBrowser();
-
-    //     const handleBeforeInstallPrompt = (e) => {
-    //         e.preventDefault();
-    //         setDeferredPrompt(e);
-    //         setInstallState(prev => ({
-    //             ...prev,
-    //             showInstallPrompt: true,
-    //         }));
-    //     };
-
-    //     window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
-    //     window.matchMedia('(display-mode: standalone)').addEventListener('change', (e) => {
-    //         setInstallState(prev => ({
-    //             ...prev,
-    //             isStandalone: e.matches,
-    //         }));
-    //     });
-
-    //     return () => {
-    //         window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
-    //     };
-    // }, [deferredPrompt]);
-
-    // const handleInstallClick = async () => {
-    //     if (deferredPrompt) {
-    //         try {
-    //             await deferredPrompt.prompt();
-    //             const result = await deferredPrompt.userChoice;
-    //             if (result.outcome === 'accepted') {
-    //                 console.log('User accepted the install prompt');
-    //                 setInstallState(prev => ({
-    //                     ...prev,
-    //                     showInstallPrompt: false,
-    //                 }));
-    //             }
-    //             setDeferredPrompt(null);
-    //         } catch (error) {
-    //             console.error('Install prompt error:', error);
-    //         }
-    //     }
-    // };
-
-    // const InstallButton = () => {
-    //     const { isIOS, isSafari, showInstallPrompt, isStandalone } = installState;
-
-    //     if (isStandalone) return null;
-
-    //     if (isIOS && isSafari && showInstallPrompt) {
-    //         return (
-    //             <div className="fixed bottom-24 right-6 flex flex-col items-end z-50">
-    //                 <button className="py-2 px-4 rounded-full bg-blue-600 text-white shadow-md mb-2">
-    //                     앱 설치하기
-    //                 </button>
-    //                 <div className="bg-white p-4 rounded-lg shadow-lg text-sm max-w-xs border border-gray-200">
-    //                     <p className="font-bold mb-2">설치 방법</p>
-    //                     <ol className="space-y-2">
-    //                         <li className="flex items-center gap-2">
-    //                             <IoShareOutline className="w-5 h-5" />
-    //                             공유 버튼을 탭하세요
-    //                         </li>
-    //                         <li>스크롤을 내려서 <strong>&quot;홈 화면에 추가&quot;</strong>를 선택하세요</li>
-    //                         <li>&quot;추가&quot;를 탭하면 설치가 완료됩니다</li>
-    //                     </ol>
-    //                 </div>
-    //             </div>
-    //         );
-    //     }
-
-    //     if (!isIOS && !isSafari && showInstallPrompt) {
-    //         return (
-    //             <button 
-    //                 onClick={handleInstallClick}
-    //                 className="fixed bottom-24 right-6 py-2 px-4 rounded-full bg-blue-600 text-white shadow-md hover:bg-blue-700 transition-colors z-50"
-    //             >
-    //                 앱 설치하기
-    //             </button>
-    //         );
-    //     }
-
-    //     return null;
-    // };
